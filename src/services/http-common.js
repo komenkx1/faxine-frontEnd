aef5b46a12d1ad0080b865f19286e73d601e5402
I komang wahyu hadi permana <44283571+komenkx1@users.noreply.github.com>
2022-06-13 15:54:31 +0800

    update api adress

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const isLoginUser =JSON.parse(localStorage.getItem('userLoginInfo'));
 
 export default axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://mwhp.me/api',
   headers: {
     'Content-type': 'application/json',
     "Authorization" : isLoginUser != null ?  `Bearer ${isLoginUser.token}` : null
